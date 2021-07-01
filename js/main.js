bf29bdf1ae51b17bae57f9c15f2e0782ab11a24c
Jay <jayparthshah786@gmail.com>
2021-07-02 02:25:47 +0530

    website v1 ready

---
+++ js/main.js
@@ -0,0 +1,83 @@
+ 
+  $(window).on("load",function(){
+
+    // ----------Preloader--------
+     $(".preloader").delay(1500).fadeOut("slow");
+    
+});
+ 
+ 
+ $(document).ready(function(){
+
+   
+
+    // -----------Navbar Shrink----------------
+    $(window).on("scroll",function(){
+        if($(this).scrollTop()>90){
+            $(".navbar").addClass("navbar-shrink");
+           
+        }else{
+            $(".navbar").removeClass("navbar-shrink");
+           
+        }
+    });
+
+
+    // -------------------  Features carousel ----------------
+    $('.features-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+              
+            },
+            600:{
+                items:2,
+            
+            },
+            1000:{
+                items:3,
+             
+            }
+        }
+    })
+
+    // -------------------  ss carousel ----------------
+    $('.screenshots-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+              
+            },
+            600:{
+                items:2,
+            
+            },
+            1000:{
+                items:4,
+             
+            }
+        }
+    })
+
+// --------------scrollit-------------------
+$.scrollIt({
+    topOffset:-50
+});
+
+
+// ------------Navbar Collapse ---------
+$(".nav-link").on("click",function(){
+
+    $(".navbar-collapse").collapse("hide");
+});
+
+
+ });
+
+ 
